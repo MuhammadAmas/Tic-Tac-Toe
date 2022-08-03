1c4273acb150ff96d624c768e59ba3db6acd803f
MuhammadAmas <amaswaseem@gmail.com>
2022-08-03 17:21:53 +0500

    Updated

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 let player1 = [null, null, null, null, null, null, null, null, null];
 let player2 = [null, null, null, null, null, null, null, null, null];
 
-document.querySelector('.start-button').addEventListener('click', startGame);
-
-for (let i = 0; i < 9; i++) {
-    document.querySelector(`.button${i+1}`).disabled = true;
+function disableButtons() {
+    for (let i = 0; i < 9; i++) {
+        document.querySelector(`.button${i + 1}`).disabled = true;
+    }
 }
 
 function startGame() {
     for (let i = 0; i < 9; i++) {
-        document.querySelector(`.button${i+1}`).disabled = false;
+        document.querySelector(`.button${i + 1}`).disabled = false;
     }
     alert("You can Start the Game!");
     document.querySelector('.start-button').style.backgroundColor = '#EAF6F6';
     document.querySelector('.start-button').style.color = '#06283D';
+}
+
+function replay(){
+    boxClear();
+    for (let i = 0; i < 9; i++) {
+        document.querySelector(`.button${i + 1}`).disabled = false;
+        player1[i] = null;
+        player2[i] = null;
+    }
 
 }
 
-function boxClear(){
+function boxClear() {
     for (let i = 0; i < 9; i++) {
-        document.querySelector(`.button${i+1}`).innerHTML = '';
+        document.querySelector(`.button${i + 1}`).innerHTML = '';
     }
 }
 
@@ -28,7 +37,7 @@ let val = 2;
 function marker() {
     if (val % 2 == 0) {
         mark = 'X';
-    } else {    
+    } else {
         mark = 'O';
     }
     val += 1
@@ -40,25 +49,24 @@ function marker() {
 function clicked(boxNumber) {
     marker();
     document.querySelector(`.button${boxNumber + 1}`).innerHTML = `${marker().mark}`;
-    
+
     if (marker().mark == 'X') {
         player1[boxNumber] = 1;
     } else {
         player2[boxNumber] = 1
     }
     document.querySelector(`.button${boxNumber + 1}`).disabled = true;
-    
 
-    
     if ((player1[0] && player1[1] && player1[2]) || (player1[3] && player1[4] && player1[5]) || (player1[6] && player1[7] && player1[8]) || (player1[0] && player1[3] && player1[6]) || (player1[1] && player1[4] && player1[7]) || (player1[2] && player1[5] && player1[8]) || (player1[0] && player1[4] && player1[8]) || (player1[2] && player1[4] && player1[6]) == 1) {
         console.log('Player1! You Won');
+        disableButtons();
         alert('Player1! You Won');
-        boxClear();
+        replay();
     } else if ((player2[0] && player2[1] && player2[2]) || (player2[3] && player2[4] && player2[5]) || (player2[6] && player2[7] && player2[8]) || (player2[0] && player2[3] && player2[6]) || (player2[1] && player2[4] && player2[7]) || (player2[2] && player2[5] && player2[8]) || (player2[0] && player2[4] && player2[8]) || (player2[2] && player2[4] && player2[6]) == 1) {
         console.log('Player2! You Won');
-        alert('Player2! You Won', boxClear());
+        disableButtons();
+        alert('Player2! You Won');
     }
-
 
     console.log('player1', player1);
     console.log('player2', player2);
@@ -68,3 +76,5 @@ function clicked(boxNumber) {
 
 
 
+document.querySelector('.start-button').addEventListener('click', startGame);
+disableButtons();
