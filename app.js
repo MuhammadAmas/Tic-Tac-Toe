e5ade1ef9e1917f5e978f2890b0f901d6ea46133
MuhammadAmas <amaswaseem@gmail.com>
2022-08-03 17:11:16 +0500

    Updated

--- app.js
+++ app.js
@@ -2,49 +2,69 @@ let player1 = [null, null, null, null, null, null, null, null, null];
 let player2 = [null, null, null, null, null, null, null, null, null];
 
 document.querySelector('.start-button').addEventListener('click', startGame);
-function startGame() {
-    alert("You can Start the Game!")
+
+for (let i = 0; i < 9; i++) {
+    document.querySelector(`.button${i+1}`).disabled = true;
 }
 
+function startGame() {
+    for (let i = 0; i < 9; i++) {
+        document.querySelector(`.button${i+1}`).disabled = false;
+    }
+    alert("You can Start the Game!");
+    document.querySelector('.start-button').style.backgroundColor = '#EAF6F6';
+    document.querySelector('.start-button').style.color = '#06283D';
+
+}
+
+function boxClear(){
+    for (let i = 0; i < 9; i++) {
+        document.querySelector(`.button${i+1}`).innerHTML = '';
+    }
+}
 
 let mark = 'O';
-let val = 0;
-let playerNumber = 1;
+let val = 2;
 function marker() {
     if (val % 2 == 0) {
-        mark = 'X'
-        playernumber = 1;
-    } else {
-        mark = 'O'
-        playernumber = 2;
-
+        mark = 'X';
+    } else {    
+        mark = 'O';
     }
     val += 1
     return {
-        mark,
-        playerNumber
+        mark
     };
 }
 
-
 function clicked(boxNumber) {
-    // console.log(boxNumber,"pressed");
-
+    marker();
     document.querySelector(`.button${boxNumber + 1}`).innerHTML = `${marker().mark}`;
-    player1[1] = 1;
-
-    pseudo = `player${playerNumber}`;
-    pseudo[boxNumber] = 1;
+    
+    if (marker().mark == 'X') {
+        player1[boxNumber] = 1;
+    } else {
+        player2[boxNumber] = 1
+    }
     document.querySelector(`.button${boxNumber + 1}`).disabled = true;
-    // console.log(arr)
+    
+
+    
+    if ((player1[0] && player1[1] && player1[2]) || (player1[3] && player1[4] && player1[5]) || (player1[6] && player1[7] && player1[8]) || (player1[0] && player1[3] && player1[6]) || (player1[1] && player1[4] && player1[7]) || (player1[2] && player1[5] && player1[8]) || (player1[0] && player1[4] && player1[8]) || (player1[2] && player1[4] && player1[6]) == 1) {
+        console.log('Player1! You Won');
+        alert('Player1! You Won');
+        boxClear();
+    } else if ((player2[0] && player2[1] && player2[2]) || (player2[3] && player2[4] && player2[5]) || (player2[6] && player2[7] && player2[8]) || (player2[0] && player2[3] && player2[6]) || (player2[1] && player2[4] && player2[7]) || (player2[2] && player2[5] && player2[8]) || (player2[0] && player2[4] && player2[8]) || (player2[2] && player2[4] && player2[6]) == 1) {
+        console.log('Player2! You Won');
+        alert('Player2! You Won', boxClear());
+    }
 
 
-    // if ((arr[0]&&arr[1]&&arr[2])||(arr[3]&&arr[4]&&arr[5])||(arr[6]&&arr[7]&&arr[8])||(arr[0]&&arr[3]&&arr[6])||(arr[1]&&arr[4]&&arr[7])||(arr[2]&&arr[5]&&arr[8])||(arr[0]&&arr[4]&&arr[8])||(arr[2]&&arr[4]&&arr[6]) == 1){
-    //     console.log('You Won');
-    // }
     console.log('player1', player1);
     console.log('player2', player2);
 }
+
+
 
 
 
